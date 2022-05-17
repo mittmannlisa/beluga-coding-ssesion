6c59ca464526d25cf3cda592fd24f2b22ce82405
Lisa Mittmann <lisa.mittmann@otto.de>
2022-05-17 09:16:26 +0200

    🎉 Initial Commit

---
+++ src/main/java/de/otto/beluga/codingsession/CodingSessionApplication.java
@@ -0,0 +1,13 @@
+package de.otto.beluga.codingsession;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingSessionApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CodingSessionApplication.class, args);
+  }
+
+}
---
+++ src/test/java/de/otto/beluga/codingsession/CodingSessionApplicationTests.java
@@ -0,0 +1,13 @@
+package de.otto.beluga.codingsession;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingSessionApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
